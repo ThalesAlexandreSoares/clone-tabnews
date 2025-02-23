47444bd996390c72ef1babd51117b66e954c3e5a
Thales Alexandre <thalesalexandre322@gmail.com>
2025-02-23 03:16:14 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Futura base de conhecimento para utilização interna do GE.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Eu te amo. Me responde com um 🩵 se funcionou, por favor. 😘</h1>
+  return <h1>Eu te amo. Me responde com um 🩵 se funcionou, por favor. 😘</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
